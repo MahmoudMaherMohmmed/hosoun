0b8520b00fcf6ec54c2ffd5484a815c72253c5dc
Mahmoud Maher Mohamed Nazer <mahmoudmaherfcih30051994@gmail.com>
2023-05-10 17:35:23 +0300

    update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,30 +37,6 @@ class AppServiceProvider extends ServiceProvider
         try {
             \DB::connection()->getPdo();
 
-
-            $code = @file_get_contents(public_path() . '/code.txt');
-
-            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-                $ip_address = @$_SERVER['HTTP_CLIENT_IP'];
-            }
-            //whether ip is from proxy
-            elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-                $ip_address = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-            }
-            //whether ip is from remote address
-            else {
-                $ip_address = @$_SERVER['REMOTE_ADDR'];
-            }
-
-            $d = \Request::getHost();
-            $domain = str_replace("www.", "", $d);
-
-            if ($domain == 'localhost' || strstr($domain, '.test') || strstr($domain, '192.168.0') || strstr($domain, 'mediacity.co.in')) {
-                // No Code
-            } else {
-                Tracker::validSettings($code, $domain, $ip_address);
-            }
-
             if (\DB::connection()->getDatabaseName() && Schema::hasTable('settings')) {
                 SeoHelper::seosettings();
             }
