878525466067f4850ae20512efe73cbb47bf6c90
Mahmoud Maher Mohamed Nazer <mahmoudmaherfcih30051994@gmail.com>
2023-06-08 18:16:25 +0300

    update website

--- resources/views/front/layouts/scripts.blade.php
+++ resources/views/front/layouts/scripts.blade.php
@@ -1,8 +1,5 @@
 {{-- Bootstrap script --}}
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
-
+<script src="{{ asset('front/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('front/js/jquery-3.6.3.min.js') }}"></script>
 <script src="{{ asset('front/js/owl.carousel.min.js') }}"></script>
 <script src="{{ asset('front/js/intlTelInput-jquery.min.js') }}"></script>
--- resources/views/front/layouts/styles.blade.php
+++ resources/views/front/layouts/styles.blade.php
@@ -10,13 +10,11 @@
 
 {{-- bootstrap --}}
 @if (in_array(Session::get('changed_language'), ['ar', 'he', 'ur', 'arc', 'az', 'dv', 'ku', 'fa']))
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.rtl.min.css"
-    integrity="sha384-WJUUqfoMmnfkBLne5uxXj+na/c7sesSJ32gI7GfCk4zO4GthUKhSEGyvQ839BC51" crossorigin="anonymous">
+  <link href="{{ asset('front/css/bootstrap.rtl.min.css') }}" rel="stylesheet" />
   <link href="{{ asset('front/css/main.css') }}" rel="stylesheet" />
   <link href="{{ asset('front/css/homepage.css') }}" rel="stylesheet" />
 @else
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link href="{{ asset('front/css/bootstrap.min.css') }}" rel="stylesheet" />
   <link href="{{ asset('front/css/main.css') }}" rel="stylesheet" />
   <link href="{{ asset('front/css/homepage.css') }}" rel="stylesheet" />
   <link href="{{ asset('front/css/ltr.css') }}" rel="stylesheet" />
